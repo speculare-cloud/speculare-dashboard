54ab3263122ced1272f6ccc852b23b7182ed4360
Martichou <martichou.andre@gmail.com>
2021-02-22 15:36:31 +0100

    Moving from speculare-server repo

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    purge: {
+        enabled: false,
+        content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}']
+    },
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
+};
