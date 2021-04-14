85b47e57e613b8e135ab96fa6a97e6ac9fe2c066
Martichou <martichou.andre@gmail.com>
2021-04-14 23:11:36 +0200

    design: add shimming effect if no items

--- src/views/Hosts.vue
+++ src/views/Hosts.vue
@@ -2,6 +2,7 @@
 	<div class="hosts">
 		<h2 class="text-lg font-medium leading-normal mb-4 text-gray-200">Groups</h2>
 		<div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 2xl:xl:grid-cols-5 gap-4 mb-8">
+			<!-- Dummy items for now, need to integrate category -->
 			<div class="flex items-center text-gray-700 bg-white dark-mode:text-gray-200 dark-mode:bg-gray-800 relative shadow px-4 py-3 rounded-lg hover:shadow-lg dark-mode:hover:bg-gray-600 dark-mode:hover:text-gray-100 hover:text-gray-900 hover:bg-gray-200">
 				<div class="flex justify-center items-center bg-red-400 rounded-full w-12 h-12">
 					<span class="h-6 w-6 material-icons cursor-default">
@@ -50,6 +51,16 @@
 
 		<h2 class="text-lg font-medium leading-normal mb-4 text-gray-200">Hosts</h2>
 		<div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 2xl:xl:grid-cols-5 gap-4 mb-8">
+			<!-- Dummy item while the real data is loading -->
+			<div v-if="!this.$store.state.hosts_values.length" class="shimming flex items-center overflow-hidden bg-white dark-mode:bg-gray-800 relative shadow py-3 rounded-lg">
+				<div class="ml-4 bg-gray-700 rounded-full w-12 h-12">
+				</div>
+				<div class="py-1 ml-4">
+					<div class="w-24 h-4 rounded-sm bg-gray-700"></div>
+					<div class="w-16 h-3 rounded-sm bg-gray-700 mt-2"></div>
+				</div>
+			</div>
+			<!-- Real data, will take the lead once loaded -->
 			<router-link v-for="item in this.$store.state.hosts_values" v-bind:key="item.uuid" :to="'/h/' + item.uuid" v-slot="{ href, navigate }" custom>
 				<a :href="href" @click="navigate">
 					<div class="flex items-center text-gray-700 bg-white dark-mode:text-gray-200 dark-mode:bg-gray-800 relative shadow px-4 py-3 rounded-lg hover:shadow-lg dark-mode:hover:bg-gray-600 dark-mode:hover:text-gray-100 hover:text-gray-900 hover:bg-gray-200">
