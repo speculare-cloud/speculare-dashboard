ada4a02c0fd78bab6294ed4f8c62ee977f818ef1
Martichou <martichou.andre@gmail.com>
2021-04-25 20:39:03 +0200

    fix height of skeleton vue

--- src/components/Graphs/Skeleton.vue
+++ src/components/Graphs/Skeleton.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="skeleton">
-		<div class="w-100 flex items-center justify-center text-xl text-gray-400" style="height: 229px">
+		<div class="w-100 flex items-center justify-center text-xl text-gray-400" style="height: 258px">
 			<h3>Loading</h3>
 		</div>
 	</div>
