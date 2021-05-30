43656e5aac12177c3ceb43d5275c297ecfb053e0
Martichou <martichou.andre@gmail.com>
2021-05-30 23:37:29 +0200

    Update style + remove unused classes

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -64,18 +64,18 @@
 		<div class="modal fixed w-full h-full top-0 left-0 items-center justify-center" :class="{'flex': open, 'hidden': !open}">
 			<div class="modal-container bg-gray-700 w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
 				<div class="modal-content py-4 text-left px-6">
-					<!--Title-->
+					<!-- Header -->
 					<div class="flex justify-between items-center pb-3">
 						<!-- TODO - Add clock svg -->
 						<p class="text-xl text-white font-bold">Quick selector</p>
-						<button class="modal-close cursor-pointer z-50" @click="open = false">
+						<button @click="open = false">
 							<svg class="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
 								<path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
 							</svg>
 						</button>
 					</div>
 
-					<!--Body-->
+					<!-- Body -->
 					<div class="flex">
 						<p class="text-gray-200">TODO</p>
 						<!-- NOTE - Assume the data is collected every seconds at first as this influence the scale -->
@@ -85,10 +85,10 @@
 						<!-- TODO - Add selection for end at -->
 					</div>
 
-					<!--Footer-->
+					<!-- Footer -->
 					<div class="flex justify-end pt-2">
-						<button class="px-4 bg-transparent p-2 rounded-lg text-green-500 hover:bg-gray-800 hover:text-green-400 mr-2">Clear</button>
-						<button class="modal-close px-4 bg-green-500 p-2 rounded-lg text-white hover:bg-green-600">Apply</button>
+						<button class="px-4 bg-transparent p-2 rounded-md text-green-500 hover:bg-gray-800 hover:text-green-400 mr-2">Clear</button>
+						<button class="px-4 bg-green-500 p-2 rounded-md text-white hover:bg-green-600">Apply</button>
 					</div>
 				</div>
 			</div>
