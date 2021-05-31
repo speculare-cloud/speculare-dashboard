56fd7ca2136efdb65e09a9e110f017bf1f3d5245
Martichou <martichou.andre@gmail.com>
2021-05-31 21:10:30 +0200

    Graphs options: almost final design (WIP)

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -65,44 +65,58 @@
 			<div class="modal-container bg-gray-700 mx-auto rounded shadow-lg z-50 overflow-y-auto">
 				<div class="modal-content py-4 text-left px-6">
 					<!-- Body -->
-					<div>
-						<p class="text-lg text-gray-100 mb-4">Range selector</p>
-						<!-- Add -->
-						<DatePicker v-model="range" :max-date="new Date()" color="teal" is-range is-dark :columns="$screens({ default: 1, lg: 2 })" is-expanded />
-						<div class="flex flex-col sm:flex-row justify-start items-center mt-4">
-							<div class="relative flex-grow w-full">
-								<svg class="text-gray-600 w-4 h-full mx-2 absolute pointer-events-none"
-									fill="none"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
-									viewBox="0 0 24 24"
-									stroke="currentColor" >
-									<path d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-								</svg>
-								<input class="flex-grow pl-8 pr-2 py-1 bg-gray-100 border rounded w-full text-gray-900" :value="range.start" />
-							</div>
-							<span class="flex-shrink-0 m-2">
-								<svg class="w-4 h-4 stroke-current text-gray-600" viewBox="0 0 24 24">
-									<path stroke-linecap="round"
-										stroke-linejoin="round"
-										stroke-width="2"
-										d="M14 5l7 7m0 0l-7 7m7-7H3"/>
-								</svg>
-							</span>
-							<div class="relative flex-grow w-full">
-								<svg class="text-gray-600 w-4 h-full mx-2 absolute pointer-events-none"
-									fill="none"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
-									viewBox="0 0 24 24"
-									stroke="currentColor">
-									<path d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-								</svg>
-								<input class="flex-grow pl-8 pr-2 py-1 bg-gray-100 border rounded w-full text-gray-900" :value="range.end" />
-							</div>
+					<div class="">
+						<div class="">
+							<p class="text-lg text-gray-100 mb-4">Quick selector</p>
+							<select class="form-select block w-full py-1">
+								<option></option>
+								<option selected="selected">Last 5 minutes</option>
+								<option>Last 15 minutes</option>
+								<option>Last 30 minutes</option>
+								<option>Last 1 hour</option>
+								<option>Last 3 hours</option>
+								<option>Last 6 hours</option>
+							</select>
 						</div>
+						<div class="mt-4">
+							<p class="text-lg text-gray-100 mb-4">Range selector</p>
+							<DatePicker v-model="range" :max-date="new Date()" color="teal" is-range is-dark>
+								<template v-slot="{ inputValue, inputEvents, isDragging }">
+									<div class="flex flex-col sm:flex-row justify-start items-center">
+										<div class="relative flex-grow">
+											<svg class="text-gray-600 w-4 h-full mx-2 absolute pointer-events-none"
+												fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+												viewBox="0 0 24 24" stroke="currentColor">
+												<path d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
+											</svg>
+											<input class="flex-grow pl-8 pr-2 py-1 bg-gray-100 border rounded w-full"
+												:class="isDragging ? 'text-gray-600' : 'text-gray-900'"
+												:value="inputValue.start" v-on="inputEvents.start" />
+										</div>
+										<span class="flex-shrink-0 m-2">
+											<svg class="w-4 h-4 stroke-current text-gray-600" viewBox="0 0 24 24">
+												<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"/>
+											</svg>
+										</span>
+										<div class="relative flex-grow">
+											<svg class="text-gray-600 w-4 h-full mx-2 absolute pointer-events-none"
+												fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+												viewBox="0 0 24 24" stroke="currentColor">
+												<path d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+											</svg>
+											<input class="flex-grow pl-8 pr-2 py-1 bg-gray-100 border rounded w-full"
+												:class="isDragging ? 'text-gray-600' : 'text-gray-900'"
+												:value="inputValue.end" v-on="inputEvents.end"/>
+										</div>
+									</div>
+								</template>
+							</DatePicker>
+						</div>
+						<!-- NOTE - Assume the data is collected every seconds at first as this influence the scale -->
+						<!-- TODO - Fetch dynamically the scale (the every seconds part of the above), it depends on the config -->
+						<!-- TODO - Add selection for "Last 5/15/30 minutes/hours + custom scale" -->
+						<!-- TODO - Add selection for begin at -->
+						<!-- TODO - Add selection for end at -->
 					</div>
 
 					<!-- Footer -->
@@ -160,6 +174,8 @@ export default {
 	data() {
 		return {
 			open: false,
+			trueRange: null,
+			trueScale: 5,
 			range: {
       			start: null,
       			end: null
@@ -169,12 +185,16 @@ export default {
 
 	methods: {
 		clearSelection: function() {
+			// Apply default value for the scale
+			this.trueScale = 5;
+			// Clear out the range selection
 			this.range = {
 				start: null,
 				end: null
 			};
 		},
 		applySelection: function() {
+			// Finally, close the modal
 			this.open = !open;
 		}
 	}
