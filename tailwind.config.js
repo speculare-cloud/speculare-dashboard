960b0d5022c0cacd5674e40a620c98865df31fb6
Martichou <m@rtin.fyi>
2021-12-08 19:07:49 +0100

    tailwind: disable JIT

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 module.exports = {
-	mode: 'jit',
-	purge: {
-		enabled: process.env.NODE_ENV,
-		content: ['./public/index.html', './src/**/*.{vue,js,ts,jsx,tsx}']
-	},
-	variants: {},
-	plugins: []
+    purge: {
+        enabled: process.env.NODE_ENV,
+        content: ['./public/index.html', './src/**/*.{vue,js,ts,jsx,tsx}']
+    },
+    variants: {},
+    plugins: []
 }
