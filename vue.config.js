0c20fa7093ec682309b2e5e14e587a437d179d4f
Martichou <m@rtin.fyi>
2022-02-19 19:23:34 +0100

    vue.config: bring back the devHost to localhost
    On WSL (at least) running npm run serve will start the debug server
    on both localhost and the WSL's network IP address.
    
    This wouldn't be an issue if Vuejs then used localhost instead
    of the WSL IP for the Websocket (hot-reload).

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,13 @@
 const MomentLocalesPlugin = require('moment-locales-webpack-plugin')
 
 module.exports = {
-	lintOnSave: false,
-	configureWebpack: {
-		plugins: [
-			new MomentLocalesPlugin()
-		]
-	}
+    lintOnSave: false,
+    devServer: {
+        host: 'localhost',
+    },
+    configureWebpack: {
+        plugins: [
+            new MomentLocalesPlugin()
+        ]
+    }
 }
