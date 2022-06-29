139e030d62a6469c41d2784e75de6ce8c9e9f9f2
Martichou <m@rtin.fyi>
2022-06-29 19:03:20 +0200

    update .env.example and dependencies

--- src/main.js
+++ src/main.js
@@ -12,9 +12,9 @@ const app = createApp(App)
 app.use(router)
 app.use(store)
 
-app.config.globalProperties.$apiBaseUrl = (process.env.VUE_APP_HTTPS === 'NO' ? 'http' : 'https') + '://' + process.env.VUE_APP_API_DOMAIN
-app.config.globalProperties.$apiAlertsUrl = (process.env.VUE_APP_HTTPS === 'NO' ? 'http' : 'https') + '://' + process.env.VUE_APP_ALERTS_DOMAIN
-app.config.globalProperties.$wsBaseUrl = (process.env.VUE_APP_WS_SECURITY === 'NO' ? 'ws' : 'wss') + '://' + process.env.VUE_APP_WS_DOMAIN
+app.config.globalProperties.$apiBaseUrl = (process.env.VUE_APP_SERVER_HTTPS === 'NO' ? 'http' : 'https') + '://' + process.env.VUE_APP_SERVER_DOMAIN
+app.config.globalProperties.$apiAlertsUrl = (process.env.VUE_APP_ALERTS_HTTPS === 'NO' ? 'http' : 'https') + '://' + process.env.VUE_APP_ALERTS_DOMAIN
+app.config.globalProperties.$wsBaseUrl = (process.env.VUE_APP_PGCDC_SECURITY === 'NO' ? 'ws' : 'wss') + '://' + process.env.VUE_APP_PGCDC_DOMAIN
 
 app.config.globalProperties.$filters = {
 	pluralize (value, number) {
